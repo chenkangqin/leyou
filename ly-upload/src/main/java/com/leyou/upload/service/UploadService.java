a5f09108ada479fc31e32b8281191a2ede261691
chenkangqin <chenkq@minstone.com.cn>
2019-04-29 17:03:11 +0800

    图片上传

--- ly-upload/src/main/java/com/leyou/upload/service/UploadService.java
+++ ly-upload/src/main/java/com/leyou/upload/service/UploadService.java
@@ -53,7 +53,7 @@ public class UploadService{
                 StorePath storePath = this.storageClient.uploadFile(
                         file.getInputStream(), file.getSize(), extension, null);
                 // 2.3、返回完整路径
-                return "http://image.leyou.com/" + storePath.getFullPath();
+                return "http://193.112.105.142/" + storePath.getFullPath();
             } catch (Exception e) {
                 return null;
             }
